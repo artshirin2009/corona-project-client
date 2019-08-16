67a11f42b2fdbaedad7620352b5ba8fde84b99b9
Agent007 <artshirin2009@gmail.com>
2019-08-16 11:58:54 +0300

    dsds

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
@@ -25,6 +26,7 @@ export default new Vuex.Store({
     isAuthorized: false,
     isAdmin: false
   },
+  plugins: [createPersistedState()],
   mutations: {
     authorize: state => state.isAuthorized = true,
     exit: state => {
